899004a3f236b55d0868cad0a19c9344a24032ec
redonearth <wona23@gmail.com>
2021-04-12 11:03:52 +0900

    TodoInput 컴포넌트 저장 기능 구현 및 스타일링

--- lv2.intermediate/vue-todo/src/components/TodoInput.vue
+++ lv2.intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,10 +1,55 @@
 <template>
-  <div>input</div>
+  <div class="input-box shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="add-container" v-on:click="addTodo">
+      <i class="fas fa-plus add-btn"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.input-box {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.input-box input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.add-container {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.add-btn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
