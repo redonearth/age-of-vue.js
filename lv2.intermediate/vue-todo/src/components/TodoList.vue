d27e05a5ee8b0bc806475d544d3683b6ca5d76e9
redonearth <wona23@gmail.com>
2021-04-13 11:36:20 +0900

    ES6 메소드 축약 적용

--- lv2.intermediate/vue-todo/src/components/TodoList.vue
+++ lv2.intermediate/vue-todo/src/components/TodoList.vue
@@ -26,10 +26,10 @@
 export default {
   props: ["propsdata"],
   methods: {
-    removeTodo: function (todoItem, index) {
+    removeTodo(todoItem, index) {
       this.$emit("removeTodoItem", todoItem, index);
     },
-    toggleComplete: function (todoItem, index) {
+    toggleComplete(todoItem, index) {
       this.$emit("toggleTodoItem", todoItem, index);
     },
   },
