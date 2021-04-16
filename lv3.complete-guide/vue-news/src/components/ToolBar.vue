572318a945c4149d794d41d39ab2fe2fa5ab6488
redonearth <wona23@gmail.com>
2021-04-16 10:40:10 +0900

    ToolBar 컴포넌트, router-link 추가

---
+++ lv3.complete-guide/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <router-link to="/news">News</router-link>
+    <router-link to="/ask">Ask</router-link>
+    <router-link to="/jobs">Jobs</router-link>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #42b883;
+  color: white;
+  display: flex;
+  padding: 8px 16px;
+  font-size: 1.5em;
+}
+header a {
+  color: white;
+  text-decoration: none;
+}
+header a:not(:last-child) {
+  margin-right: 16px;
+}
+header .router-link-exact-active {
+  color: #354953;
+}
+</style>
