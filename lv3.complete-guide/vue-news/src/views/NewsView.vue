c1734614b26de642034aec362e920f6b591548d4
redonearth <wona23@gmail.com>
2021-04-19 13:29:52 +0900

    Axios를 이용한 API 호출

--- lv3.complete-guide/vue-news/src/views/NewsView.vue
+++ lv3.complete-guide/vue-news/src/views/NewsView.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>News</div>
+  <div>
+    <div v-for="article in news">{{ article.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      news: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => {
+        console.log(response);
+        this.news = response.data;
+      })
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <style>
