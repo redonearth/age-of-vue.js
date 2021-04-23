b62f886d9073315a4f6e89fd160653214da47414
Jisang Hong <wona23@gmail.com>
2021-04-23 15:06:31 +0900

    font-awesome 라이브러리 추가

--- lv3.complete-guide/vue-news/src/main.js
+++ lv3.complete-guide/vue-news/src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import { router } from "./routes/index.js";
 import { store } from "./store/index.js";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUser);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
