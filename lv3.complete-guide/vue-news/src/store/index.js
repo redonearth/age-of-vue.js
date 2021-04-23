8284389c6cc289139b9277c0939e16e533eb185e
redonearth <wona23@gmail.com>
2021-04-23 10:23:17 +0900

    Destructuring assignment 적용

--- lv3.complete-guide/vue-news/src/store/index.js
+++ lv3.complete-guide/vue-news/src/store/index.js
@@ -22,24 +22,24 @@ export const store = new Vuex.Store({
     },
   },
   actions: {
-    FETCH_NEWS(context) {
+    FETCH_NEWS({ commit }) {
       fetchNewsList()
-        .then((response) => {
-          context.commit("SET_NEWS", response.data);
+        .then(({ data }) => {
+          commit("SET_NEWS", data);
         })
         .catch((error) => console.log(error));
     },
-    FETCH_JOBS(context) {
+    FETCH_JOBS({ commit }) {
       fetchJobsList()
-        .then((response) => {
-          context.commit("SET_JOBS", response.data);
+        .then(({ data }) => {
+          commit("SET_JOBS", data);
         })
         .catch((error) => console.log(error));
     },
-    FETCH_ASK(context) {
+    FETCH_ASK({ commit }) {
       fetchAskList()
-        .then((response) => {
-          context.commit("SET_ASK", response.data);
+        .then(({ data }) => {
+          commit("SET_ASK", data);
         })
         .catch((error) => console.log(error));
     },
