053cfd1d58a795377caf59192e152f3e1405d8d2
Jisang Hong <wona23@gmail.com>
2021-04-23 15:07:06 +0900

    질문 상세 페이지 스타일링 적용

--- lv3.complete-guide/vue-news/src/views/ItemView.vue
+++ lv3.complete-guide/vue-news/src/views/ItemView.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
-    <p>{{ itemInfo.title }}</p>
-    <div>{{ itemInfo.content }}</div>
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user-container">
+        <div>
+          <font-awesome-icon
+            :icon="['fas', 'user']"
+            class="user-icon"
+          ></font-awesome-icon>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${itemInfo.user}`">
+            {{ itemInfo.user }}
+          </router-link>
+          <div class="time">{{ itemInfo.time_ago }}</div>
+        </div>
+      </div>
+      <h2>{{ itemInfo.title }}</h2>
+    </section>
+    <section>
+      <!-- 질문 내용 -->
+      <div v-html="itemInfo.content"></div>
+    </section>
+    <section>
+      <!-- 질문 댓글 -->
+    </section>
   </div>
 </template>
 
@@ -19,5 +42,22 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+
+.user-icon {
+  font-size: 2.5rem;
+}
+
+.user-description {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
 </style>
