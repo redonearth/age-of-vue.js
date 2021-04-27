07bce2f98e15a6fa39e1e51cac32d042b16e8b4e
Jisang Hong <wona23@gmail.com>
2021-04-27 12:34:15 +0900

    차트 라이브러리 프로젝트 생성

---
+++ lv3.complete-guide/vue-charts/chart-lib/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from "./components/BarChart.vue";
+
+export default {
+  components: {
+    BarChart,
+  },
+};
+</script>
+
+<style>
+</style>
