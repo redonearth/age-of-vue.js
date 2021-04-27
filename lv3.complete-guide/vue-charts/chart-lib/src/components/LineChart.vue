f0c87e8f0c465f9c1d980480e17338d8e34a837c
Jisang Hong <wona23@gmail.com>
2021-04-27 12:35:42 +0900

    LineChart 컴포넌트 추가

---
+++ lv3.complete-guide/vue-charts/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <canvas id="lineChart"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  mounted() {
+    const ctx = document.getElementById("lineChart").getContext("2d");
+    const chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255,99,132)",
+            borderColor: "rgb(255,99,132)",
+            data: [0, 10, 5, 2, 20, 30, 45],
+          },
+        ],
+      },
+      options: {},
+    });
+  },
+};
+</script>
+
+<style>
+</style>
