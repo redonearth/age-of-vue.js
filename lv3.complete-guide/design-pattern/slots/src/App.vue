bc037f95faaa811e0f151e54cf235066391f56e6
Jisang Hong <wona23@gmail.com>
2021-06-01 14:32:43 +0900

    디자인 패턴 - 슬롯 구현 및 활용 학습

---
+++ lv3.complete-guide/design-pattern/slots/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템 1</item>
+      <item>
+        아이템 2
+        <button>클릭</button>
+      </item>
+      <item>
+        <div>아이템 3</div>
+        <img src="./assets/logo.png" alt="Vue.js" />
+      </item>
+      <item>
+        <div style="color: blue; font-size: 2.5rem">아이템 4</div>
+      </item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  components: {
+    Item,
+  },
+};
+</script>
