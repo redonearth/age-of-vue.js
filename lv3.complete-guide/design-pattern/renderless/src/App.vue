b7a1685add1302327db3ff0b3f016263788279fd
Jisang Hong <wona23@gmail.com>
2021-06-01 15:29:49 +0900

    디자인 패턴 - Renderless Component 구현 및 활용 학습

---
+++ lv3.complete-guide/design-pattern/renderless/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <div slot-scope="{ response, loading }">
+        <div v-if="!loading">
+          <p>Name: {{ response.name }}</p>
+          <p>E-mail: {{ response.email }}</p>
+        </div>
+        <div v-if="loading">Loading...</div>
+      </div>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from "./components/FetchData.vue";
+
+export default {
+  name: "app",
+  components: {
+    FetchData,
+  },
+};
+</script>
