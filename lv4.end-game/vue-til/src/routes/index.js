286094f71f125ba148a229853983a656dd6b2ec9
Jisang Hong <wona23@gmail.com>
2021-06-18 09:40:25 +0900

    초기 진입 페이지 설정 및 없는 주소 접근 처리 적용

--- lv4.end-game/vue-til/src/routes/index.js
+++ lv4.end-game/vue-til/src/routes/index.js
@@ -8,12 +8,20 @@ Vue.use(VueRouter);
 export default new VueRouter({
   routes: [
     {
+      path: '/',
+      redirect: '/login',
+    },
+    {
       path: '/login',
       component: () => import('@/views/LoginPage.vue'),
     },
     {
       path: '/signup',
       component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
