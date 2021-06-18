9904e7a4c5e077902f98345325108419ddde51c3
Jisang Hong <wona23@gmail.com>
2021-06-18 10:18:51 +0900

    회원 가입 폼 마크업

---
+++ lv4.end-game/vue-til/src/components/SignupForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <div>
+      <label for="username">아이디 : </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">패스워드 : </label>
+      <input id="password" type="text" />
+    </div>
+    <div>
+      <label for="nickname">닉네임 : </label>
+      <input id="nickname" type="text" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
