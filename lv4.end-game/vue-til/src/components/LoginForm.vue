cf3f73b296d3f8b2c2256357a968eacffd9539d2
Jisang Hong <wona23@gmail.com>
2021-06-21 10:52:53 +0900

    로그인 폼 데이터 바인딩 및 이벤트 연결

--- lv4.end-game/vue-til/src/components/LoginForm.vue
+++ lv4.end-game/vue-til/src/components/LoginForm.vue
@@ -1,19 +1,29 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="username">아이디 : </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">패스워드 : </label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {},
+  },
+};
 </script>
 
 <style></style>
