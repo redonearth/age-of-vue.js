ca8c487edb7341ba23d4ebfe6ab6bf8a424e53ca
Jisang Hong <wona23@gmail.com>
2021-06-21 09:21:10 +0900

    회원 가입 폼 데이터 바인딩 및 이벤트 연결

--- lv4.end-game/vue-til/src/components/SignupForm.vue
+++ lv4.end-game/vue-til/src/components/SignupForm.vue
@@ -1,23 +1,36 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="username">아이디 : </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">패스워드 : </label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <div>
       <label for="nickname">닉네임 : </label>
-      <input id="nickname" type="text" />
+      <input id="nickname" type="text" v-model="nickname" />
     </div>
-    <button type="submit">로그인</button>
+    <button type="submit">회원 가입</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      nickname: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('폼 제출');
+    },
+  },
+};
 </script>
 
 <style></style>
