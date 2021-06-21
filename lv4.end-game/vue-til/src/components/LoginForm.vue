1505c702acea8b6e0f004dfe77fd3393307bc008
Jisang Hong <wona23@gmail.com>
2021-06-21 10:44:48 +0900

    로그인 폼 컴포넌트 생성 및 마크업

---
+++ lv4.end-game/vue-til/src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div>
+      <label for="username">아이디 : </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">패스워드 : </label>
+      <input id="password" type="text" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
