cbafaf5f3a09c19f84da0c5ed9875cf88a071c6d
Jisang Hong <wona23@gmail.com>
2021-06-21 10:53:12 +0900

    로그인 API 함수 생성

--- lv4.end-game/vue-til/src/api/index.js
+++ lv4.end-game/vue-til/src/api/index.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: process.env.VUE_APP_API_URL,
 });
 
 function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
-export { registerUser };
+function loginUser(userData) {
+  return instance.post('login', userData);
+}
+
+export { registerUser, loginUser };
