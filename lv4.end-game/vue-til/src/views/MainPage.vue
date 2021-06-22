4a4dea40b1ea67428c617e521d81e8ed27632f6b
Jisang Hong <wona23@gmail.com>
2021-06-22 14:41:53 +0900

    학습 노트 목록 아이템 컴포넌트화

--- lv4.end-game/vue-til/src/views/MainPage.vue
+++ lv4.end-game/vue-til/src/views/MainPage.vue
@@ -3,26 +3,24 @@
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
       <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          <div class="post-title">
-            {{ postItem.title }}
-          </div>
-          <div class="post-contents">
-            {{ postItem.contents }}
-          </div>
-          <div class="post-time">
-            {{ postItem.createdAt }}
-          </div>
-        </li>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
 import { fetchPosts } from '@/api/index';
 
 export default {
+  components: {
+    PostListItem,
+  },
   data() {
     return {
       postItems: [],
