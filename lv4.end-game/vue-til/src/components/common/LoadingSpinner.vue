a59848123c07ba0d3e100415da0c1b9e5632d5fe
Jisang Hong <wona23@gmail.com>
2021-06-22 14:50:43 +0900

    데이터 로딩 상태 표시 추가

---
+++ lv4.end-game/vue-til/src/components/common/LoadingSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="spinner-container">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.spinner-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 240px;
+}
+.spinner {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid #e0e0e0;
+  border-bottom: 5px solid #1629fe;
+  animation: spin 1s linear infinite;
+  position: relative;
+}
+</style>
