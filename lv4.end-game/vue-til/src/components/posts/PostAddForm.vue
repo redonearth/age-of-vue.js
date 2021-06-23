435f7e2227d65fb7af7fd85d8e5eca077274950b
Jisang Hong <wona23@gmail.com>
2021-06-23 09:38:28 +0900

    학습 노트 등록 버튼, 폼 컴포넌트 생성 및 라우터 연결

---
+++ lv4.end-game/vue-til/src/components/posts/PostAddForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contents">
+    <div class="form-wrapper">
+      <form class="form">
+        <div>
+          <label for="title">제목</label>
+          <input id="title" type="text" />
+        </div>
+        <div>
+          <label for="contents">내용</label>
+          <textarea id="contents" type="text" rows="10" />
+        </div>
+        <button type="submit" class="btn">등록하기</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.form-wrapper .form {
+  width: 100%;
+}
+.btn {
+  color: white;
+}
+</style>
---
+++ lv4.end-game/vue-til/src/views/PostAddPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="page-header">포스트 등록</h1>
+    <PostAddForm></PostAddForm>
+  </div>
+</template>
+
+<script>
+import PostAddForm from '@/components/posts/PostAddForm.vue';
+
+export default {
+  components: {
+    PostAddForm,
+  },
+};
+</script>
+
+<style></style>
