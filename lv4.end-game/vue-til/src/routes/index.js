2d179c6e812c3142372da370b852e9c1a98395eb
Jisang Hong <wona23@gmail.com>
2021-06-23 11:02:48 +0900

    API 모듈화 적용

--- lv4.end-game/vue-til/src/routes/index.js
+++ lv4.end-game/vue-til/src/routes/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// import LoginPage from '@/views/LoginPage.vue';
-// import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
