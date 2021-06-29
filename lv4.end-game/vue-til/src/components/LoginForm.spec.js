77165ad825c2d8db7cd514986533f1162c409304
Jisang Hong <wona23@gmail.com>
2021-06-29 10:43:10 +0900

    로그인 컴포넌트 테스트 코드 작성

--- lv4.end-game/vue-til/src/components/LoginForm.spec.js
+++ lv4.end-game/vue-til/src/components/LoginForm.spec.js
@@ -2,16 +2,28 @@ import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('ID는 이메일 형식이어야 한다.', () => {
+  test('ID가 이메일 형식이 아니면 경고 메시지가 출력된다.', () => {
     const wrapper = shallowMount(LoginForm, {
       data() {
         return {
-          username: 'test@test.com',
+          username: 'test',
         };
       },
     });
-    const idInput = wrapper.find('#username');
-    console.log('Input의 값 : ', idInput.element.value);
-    console.log(wrapper.vm.isUsernameValid);
+    const warningText = wrapper.find('.warning');
+    expect(warningText.exists()).toBeTruthy();
+  });
+
+  test('ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화된다.', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'a@a.com',
+          password: '1234',
+        };
+      },
+    });
+    const button = wrapper.find('button');
+    expect(button.element.disabled).toBeTruthy();
   });
 });
