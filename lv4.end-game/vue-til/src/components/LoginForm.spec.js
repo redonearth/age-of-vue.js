7e7240ce4e2d1482257672633728ec7713adb85a
Jisang Hong <wona23@gmail.com>
2021-06-29 10:07:46 +0900

    이메일 유효성 검사 기능 동작 테스트 코드로 확인

--- lv4.end-game/vue-til/src/components/LoginForm.spec.js
+++ lv4.end-game/vue-til/src/components/LoginForm.spec.js
@@ -2,14 +2,16 @@ import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    const wrapper = shallowMount(LoginForm);
-    expect(wrapper.vm.username).toBe('');
-  });
-
   test('ID는 이메일 형식이어야 한다.', () => {
-    const wrapper = shallowMount(LoginForm);
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test@test.com',
+        };
+      },
+    });
     const idInput = wrapper.find('#username');
-    console.log(idInput.html());
+    console.log('Input의 값 : ', idInput.element.value);
+    console.log(wrapper.vm.isUsernameValid);
   });
 });
